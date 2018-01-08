58a480b26173baf2c2a24b84d4433c22b82616ed
whirlwin <whirlwin90@gmail.com>
2018-01-08 19:02:38 +0100

    Add more info about deploys

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -5,65 +5,105 @@ const APP_PORT = process.env.APP_PORT;
 const app = express({viewEngine: "pug"});
 
 // TODO replace with persistent db
-const fooAppDeploys = [
-    {
-        commitHash: "ab1c23",
-        commitMessage: "DIG-123: Endre farge på knapp",
-        status: "not-deployed",
-        deploy: null
-    },
-    {
-        commitHash: "1bace3",
-        commitMessage: "DIG-112: Legge til knapp",
-        status: "not-deployed",
-        deploy: null
-    },
-    {
-        commitHash: "c43b11",
-        commitMessage: "DIG-100: Legge til tekst",
-        status: "deployed",
-        deploy: {
-            timestamp: "13-06-2018",
-            destinations: [
-              {
-                clusterName: "prod01.sparebank1.no",
-                pods: [
-                  {
-                    name: "foo-app-ac21",
+const commits = [{
+    commitHash: "ab1c23",
+    commitMessage: "DIG-123: Endre farge på knapp",
+    environments: {
+        test: {
+            timestamp: null,
+            clusters: [{
+                name: "test01.sparebank1.no",
+                pods: [{
+                    name: "foo-app-90cca",
                     status: "running"
-                  },
-                  {
-                    name: "foo-app-29bb",
-                    status: "starting"
-                  }
-                ]
-
-              },
-              {
-                clusterName: "prod02.sparebank1.no",
-                pods: [
-                  {
-                    name: "foo-app-ah5c",
-                    status: "running"
-                  },
-                  {
-                    name: "foo-app-923a",
-                    status: "failed"
-                  }
-                ]
-
-              }
-            ],
+                }]
+            }]
         }
     }
-];
+}, {
+    commitHash: "1bace3",
+    commitMessage: "DIG-112: Legge til knapp",
+    environments: {
+    }
+}, {
+    commitHash: "c43b11",
+    commitMessage: "DIG-100: Legge til tekst",
+    environments: {
+        test: {
+            timestamp: "13-06-2018",
+            clusters: [{
+                name: "test01.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ac21",
+                    status: "running"
+                }, {
+                    name: "foo-app-29bb",
+                    status: "starting"
+                }]
+            }, {
+                name: "test02.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ah5c",
+                    status: "running"
+                }, {
+                    name: "foo-app-923a",
+                    status: "failed"
+                }]
+            }]
+        },
+        qa: {
+            timestamp: "13-06-2018",
+            clusters: [{
+                name: "test01.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ac21",
+                    status: "running"
+                }, {
+                    name: "foo-app-29bb",
+                    status: "starting"
+                }]
+            }, {
+                name: "test02.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ah5c",
+                    status: "running"
+                }, {
+                    name: "foo-app-923a",
+                    status: "failed"
+                }]
+            }]
+        },
+        prod: {
+            timestamp: "13-06-2018",
+            clusters: [{
+                name: "test01.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ac21",
+                    status: "running"
+                }, {
+                    name: "foo-app-29bb",
+                    status: "starting"
+                }]
+            }, {
+                name: "test02.sparebank1.no",
+                pods: [{
+                    name: "foo-app-ah5c",
+                    status: "running"
+                }, {
+                    name: "foo-app-923a",
+                    status: "failed"
+                }]
+            }]
+        }
+    }
+}];
 
 app.set("view engine", "pug");
 app.set("views", "./src/main/views");
 app.use("/static", express.static("src/main/public"));
 
 app.get("/:app", (req, res) => {
-    res.render("app", {fooAppDeploys: fooAppDeploys});
+    res.render("app", {commits: commits});
 });
 
 app.get("/healthcheck", (req, res) => {
